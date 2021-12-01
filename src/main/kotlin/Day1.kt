57381c19965ba41fde40056971e09875fca735b3
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2021-12-01 21:07:08 +0100

    day 1 refactor

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -2,12 +2,11 @@ class Day1(input: List<String>) {
 
     private val measurements = input.map { measurement -> measurement.toInt() }
 
-    fun part1() = measurements
-        .zipWithNext()
-        .count { (current, next) -> next > current }
+    fun part1() = solve(1)
 
-    fun part2() = measurements.windowed(3)
-        .map { group -> group.sum() }
-        .zipWithNext()
-        .count { (current, next) -> next > current }
+    fun part2() = solve(3)
+
+    private fun solve(windowSize: Int) = measurements
+        .windowed(windowSize).map { group -> group.sum() }
+        .zipWithNext().count { (current, next) -> next > current }
 }
