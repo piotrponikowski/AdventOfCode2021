c2476602a0a25c50e975da23a72fd8665dcd1373
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2021-12-09 18:12:41 +0100

    day 9  refactor

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -7,35 +7,27 @@ class Day9(input: List<String>) {
 
     fun part1() = lowPoints().values.sumOf { value -> value + 1 }
 
-    fun part2() = lowPoints().keys.map { lowPoint ->
-        val pointsForCheck = mutableListOf(lowPoint)
-        val basin = mutableSetOf(lowPoint)
+    fun part2() = lowPoints().keys
+        .map { lowPoint -> groupBasin(setOf(lowPoint), listOf(lowPoint)).size }
+        .sorted().reversed().take(3).reduce { current, next -> current * next }
 
-        while (pointsForCheck.isNotEmpty()) {
-            val current = pointsForCheck.removeFirst()
-            val currentValue = points[current]!!
+    private fun lowPoints() = points.filter { (point, value) ->
+        directions.all { direction -> (points[point + direction] ?: 9) > value }
+    }
 
-            neighbours.map { neighbour -> current + neighbour }
-                .map { other -> other to (points[other] ?: 9) }
-                .filter { (other, otherValue) -> otherValue in currentValue..8 && other !in basin }
-                .forEach { (other, _) ->
-                    pointsForCheck += other
-                    basin += other
-                }
+    private fun groupBasin(basin: Set<Point>, pointsForCheck: List<Point>): Set<Point> {
+        return if (pointsForCheck.isEmpty()) basin
+        else {
+            val nextPoints = pointsForCheck.flatMap { current ->
+                directions.map { direction -> current + direction }
+                    .filter { neighbour -> neighbour !in basin }
+                    .filter { neighbour -> (points[neighbour] ?: 9) < 9 }
+            }
+            groupBasin(basin + nextPoints, nextPoints)
         }
-        basin.size
-    }.sorted().reversed().take(3).reduce { a, b -> a * b }
+    }
 
-    private fun lowPoints() =
-        points.filter { (point, value) -> neighbours.all { neighbour -> (points[point + neighbour] ?: 9) > value } }
-
-
-    private val neighbours = listOf(
-        Point(-1, 0),
-        Point(1, 0),
-        Point(0, -1),
-        Point(0, 1)
-    )
+    private val directions = listOf(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Point) = Point(x + other.x, y + other.y)
