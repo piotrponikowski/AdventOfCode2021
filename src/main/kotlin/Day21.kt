13f3202555c0e15b04ca9b4c6d37831a455854d6
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2021-12-21 08:56:09 +0100

    day 21 part 2

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 class Day21(val p1: Int, p2: Int) {
     val init1 = Player(p1, 0)
     val init2 = Player(p2, 0)
@@ -12,17 +14,17 @@ class Day21(val p1: Int, p2: Int) {
     }.iterator()
 
     fun part1() = loop()
-    
-    fun loop() : Long {
+
+    fun loop(): Long {
         var player1 = init1
         var player2 = init2
 
         var turns = 0L
-        while(true) {
+        while (true) {
             player1 = stepPlayer(player1)
             turns++
-            
-            if(player1.score >= 1000) {
+
+            if (player1.score >= 1000) {
                 return player2.score * turns * 3
             }
 
@@ -30,29 +32,88 @@ class Day21(val p1: Int, p2: Int) {
             turns++
 
 
-            if(player2.score >= 1000) {
+            if (player2.score >= 1000) {
                 return player1.score * turns * 3
             }
         }
     }
 
-    private fun stepPlayer(start: Player) :Player {
+    val range = (1..3)
+ 
+    data class State(val player1: Player,val player2: Player, val currentPlayer: Int)
+
+    val cache = mutableMapOf<State, Pair<Long,Long>>()
+    
+    fun loop2(player1: Player, player2: Player, currentPlayer: Int = 1) : Pair<Long,Long> {
+        val key = State(player1, player2, currentPlayer)
+
+        if(cache.containsKey(key)) {
+            return cache[key]!!
+        } 
         
+        var wins1 = 0L
+        var wins2 = 0L
+        
+        for (dice1 in range) {
+            for (dice2 in range) {
+                for (dice3 in range) {
+                    if (currentPlayer == 1) {
+                        var position1 = player1.position + dice1 + dice2 + dice3
+                        while (position1 > 10) position1 -= 10
+
+                        val score1 = player1.score + position1
+
+                        if (score1 >= 21) {
+                            wins1++
+                        } else {
+                            val (w1, w2) = loop2(Player(position1, score1), player2, 2)
+                            cache[State(Player(position1, score1), player2, 2)] = w1 to w2
+                            
+                            wins1 += w1
+                            wins2 += w2
+                        }
+                    } else {
+                        var position2= player2.position + dice1 + dice2 + dice3
+                        while (position2 > 10) position2 -= 10
+
+                        val score2 = player2.score + position2
+
+                        if (score2 >= 21) {
+                            wins2++
+                        } else {
+                            val (w1, w2) = loop2(player1, Player(position2, score2), 1)
+                            cache[State(player1, Player(position2, score2), 1)] = w1 to w2
+                            
+                            wins1 += w1
+                            wins2 += w2
+                        }
+                    }
+                }
+            }
+        }
+        return wins1 to wins2
+    }
+
+    private fun stepPlayer(start: Player): Player {
+
         var position = start.position + dice.next() + dice.next() + dice.next()
-        while(position > 10) position -= 10
-        
-        
-        val score =  start.score + position
+        while (position > 10) position -= 10
+
+
+        val score = start.score + position
         println("start: ${start.position}, position: $position, score: $score")
         return Player(position, score)
     }
 
     data class Player(val position: Int, val score: Int)
 
-    fun part2() = 2
+    fun part2()  {
+        val (r1, r2) = loop2(init1, init2, 1)
+        println(max(r1, r2))
+    }
 }
 
 fun main() {
     val input = readLines("day21.txt", true)
-    println(Day21(2, 8).part1())
+    println(Day21(2, 8).part2())
 }
